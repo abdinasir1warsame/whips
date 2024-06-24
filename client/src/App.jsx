5b2a00b6ff61bdb6aa2621ee7cfb4efdade56ece
abdinasir1993 <abdinasir_jirde@hotmail.com>
2024-06-24 23:31:35 +0100

    url adjustments.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,60 +1,48 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
+import IndexPage from './pages/indexPage';
+import LoginPage from './pages/loginPage';
+import SignUpPage from './pages/signupPage';
+import Layout from './layout';
+import CarsPage from './pages/carsPage';
+import UserContextProvider from './userContext';
 
-import './App.css'
-import {Route, Routes} from "react-router-dom"
-import AOS from "aos";
-import "aos/dist/aos.css";
+import axios from 'axios';
+import ProfilePage from './pages/accountsPage';
+import AllCars from './pages/allCars';
+import CarsFormPage from './pages/carsFormPage';
+import CarPage from './pages/carPage';
+import BookingsPage from './pages/bookingsPage';
+import BookingPage from './pages/bookingPage';
 
-
-import IndexPage from './pages/indexPage'
-import LoginPage from './pages/loginPage'
-import SignUpPage from './pages/signupPage'
-import Layout from './layout'
-import CarsPage from './pages/carsPage'
-import UserContextProvider from './userContext'
-
-
-import axios from 'axios'
-import ProfilePage from './pages/accountsPage'
-import AllCars from './pages/allCars'
-import CarsFormPage from './pages/carsFormPage'
-import CarPage from './pages/carPage'
-import BookingsPage from './pages/bookingsPage'
-import BookingPage from './pages/bookingPage'
-
-
-
-axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
-  
   AOS.init();
 
   return (
-
- <UserContextProvider>
-    <Routes>
-      <Route path='/' element={<Layout/>}>
-   
-      <Route index element={<IndexPage/>}/>
-      <Route path="/login" element={<LoginPage/>}/>
-      <Route path="/signup" element={<SignUpPage/>}/>
-      <Route path="/cars" element={<AllCars/>}/>
-      <Route path="/account/:subpage?" element={<ProfilePage/>}/>
-      <Route path="/account/cars" element={<CarsPage/>}/>
-      <Route path="/account/cars/new" element={<CarsFormPage/>}/>
-      <Route path="/account/cars/:id" element={<CarsFormPage/>}/>
-      <Route path="/car/:id" element={<CarPage/>}/>
-      <Route path="/account/bookings" element={<BookingsPage/>}/>
-      <Route path="/account/bookings/:id" element={<BookingPage/>}/>
-  
-      
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/cars" element={<AllCars />} />
+          <Route path="/account/:subpage?" element={<ProfilePage />} />
+          <Route path="/account/cars" element={<CarsPage />} />
+          <Route path="/account/cars/new" element={<CarsFormPage />} />
+          <Route path="/account/cars/:id" element={<CarsFormPage />} />
+          <Route path="/car/:id" element={<CarPage />} />
+          <Route path="/account/bookings" element={<BookingsPage />} />
+          <Route path="/account/bookings/:id" element={<BookingPage />} />
+        </Route>
+      </Routes>
     </UserContextProvider>
-
-  )
+  );
 }
 
-export default App
+export default App;
