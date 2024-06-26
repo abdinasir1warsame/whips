2e596740c90aa017ded83da1bac68117657c07a9
abdinasir1993 <abdinasir_jirde@hotmail.com>
2024-06-26 01:49:19 +0100

    LOGIN AMMENDED ON NAVBAR

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -123,7 +123,7 @@ const Navbar = () => {
                       onClick={handleLinkClick}
                       className=" border border-white px-4 py-2 shadow rounded-2xl cursor-pointer "
                     >
-                      <label>GET STARTED</label>
+                      <label>LOGIN</label>
                     </Link>
                     <Link
                       to="/signup"
